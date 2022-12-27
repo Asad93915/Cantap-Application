0d00bd18e38cecedd9f71492b179876b48d3262c
Asad93915 <asadaligujjar110@gmail.com>
2022-12-27 19:12:08 +0500

    for backup1

--- lib/configs/apis_config.dart
+++ lib/configs/apis_config.dart
@@ -3,7 +3,6 @@ const String baseUrl="https://svm.impliessolutions.com/api/";
 const String loginUrl=baseUrl + "Authenticate/login";
 //Classes
 const String getClassUrl=baseUrl+"Standard/getList";
-
 //Admin
 const String adminCreateClassUrl=baseUrl+ "School/saveUser";
 const String getClassesUrl=baseUrl + "Standard/getListByActiveUser";
